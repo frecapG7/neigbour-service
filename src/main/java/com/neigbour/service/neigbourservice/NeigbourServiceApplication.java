3f63fee36d5ec73b45bcb5ecb4c04a496e24dc03
RECAPE Florian <florian.recape@wanadoo.fr>
2018-12-12 21:11:22 -0500

    Inital commit for Neigbour project Contains first draft of API service

---
+++ src/main/java/com/neigbour/service/neigbourservice/NeigbourServiceApplication.java
@@ -0,0 +1,14 @@
+package com.neigbour.service.neigbourservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeigbourServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeigbourServiceApplication.class, args);
+	}
+
+}
+
