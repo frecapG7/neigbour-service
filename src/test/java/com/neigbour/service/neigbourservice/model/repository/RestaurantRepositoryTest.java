3e9f7f287c5c166a2eda2ae9e552187046eb422c
RECAPE Florian <florian.recape@wanadoo.fr>
2018-12-13 01:16:36 -0500

    Unit test for restaurant respository

--- src/test/java/com/neigbour/service/neigbourservice/model/repository/RestaurantRepositoryTest.java
+++ src/test/java/com/neigbour/service/neigbourservice/model/repository/RestaurantRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.neigbour.service.neigbourservice.model.entity.Restaurant;
+import com.neigbour.service.neigbourservice.util.TestConstants;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -16,30 +17,39 @@ public class RestaurantRepositoryTest {
 
 	@Autowired
 	TestEntityManager testEntityManager;
-	
+
 	@Autowired
 	RestaurantRepository restaurantRepository;
 	
+	
+
 	@Test
-	public void should_find_two_restaurant(){
-		//Create mock data for insert
-		Restaurant restaurant1 = Restaurant
-				.builder()
-				.name("restaurant1")
-				.description("restaurant 1")
-				.address("test")
+	public void should_find_two_restaurant() {
+		// Create mock data for insert
+		Restaurant restaurant1 = Restaurant.builder().name("restaurant1").description("restaurant 1").address("test")
 				.build();
-		Restaurant restaurant2 = Restaurant
-				.builder()
-				.name("restaurant2")
-				.description("restaurant 2")
-				.address("test")
+		Restaurant restaurant2 = Restaurant.builder().name("restaurant2").description("restaurant 2").address("test")
 				.build();
-		
+
 		testEntityManager.persist(restaurant1);
 		testEntityManager.persist(restaurant2);
-		
-		Assert.assertEquals(2 , restaurantRepository.count());
+
+		Assert.assertEquals(2, restaurantRepository.count());
+	}
+	
+	@Test
+	public void should_save_a_restaurant(){
+		Restaurant restaurant = restaurantRepository.save(
+				Restaurant
+				.builder()
+				.name(TestConstants.restaurantNameTest)
+				.description(TestConstants.restaurantDescriptionTest)
+				.address(TestConstants.restaurantAddressTest)
+				.build()
+				);
+		Assert.assertEquals(TestConstants.restaurantNameTest, restaurant.getName());
+		Assert.assertEquals(TestConstants.restaurantDescriptionTest, restaurant.getDescription());
+		Assert.assertEquals(TestConstants.restaurantAddressTest, restaurant.getAddress());
 		
 	}
 }
---
+++ src/test/java/com/neigbour/service/neigbourservice/util/TestConstants.java
@@ -0,0 +1,12 @@
+package com.neigbour.service.neigbourservice.util;
+
+public final class TestConstants {
+
+	public static final String restaurantNameTest = "restaurant test";
+	
+	public static final String restaurantAddressTest = "address-test";
+	
+	public static final String restaurantDescriptionTest = "blablablablabla";
+	
+	
+}
